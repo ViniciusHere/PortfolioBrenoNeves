4f0ca4d5beeefd462ef41da8a21118d1004250e6
vinicius gabriel <v.souza@Outlook.pt>
2024-09-03 16:35:10 -0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path: '/portfolioBrenoNeves',
+    path: 'https://viniciushere.github.io/portfolioBrenoNeves/',
     element: <Home/>,
   },
   {
