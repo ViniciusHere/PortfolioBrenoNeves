7939a3b8e73a14ee4b1e8e5ebd5bfbf5449a1287
vinicius gabriel <v.souza@Outlook.pt>
2024-09-06 16:43:38 -0300

    add: Componentes pagina Nami

--- src/pages/WorkNami.jsx
+++ src/pages/WorkNami.jsx
@@ -1,9 +1,21 @@
 import Header from '../components/Header'
+import Frequency from '../components/Frequency'
 import PagesTitle from '../components/PagesTitle'
 import ResumeProject from '../components/ResumeProject'
 import cover from '../assets/workNami/Cover.png'
 import cover2 from '../assets/workNami/Cover2.png'
-import { useState } from 'react'
+import Frequency1 from '../assets/workNami/Frequency1.png'
+import Frequency2 from '../assets/workNami/Frequency1.png'
+import Frequency3 from '../assets/workNami/Frequency1.png'
+import Frequency4 from '../assets/workNami/Frequency1.png'
+import Frequency5 from '../assets/workNami/Frequency1.png'
+import Frequency6 from '../assets/workNami/Frequency1.png'
+import Frequency7 from '../assets/workNami/Frequency1.png'
+import Frequency8 from '../assets/workNami/Frequency1.png'
+import Frequency9 from '../assets/workNami/Frequency1.png'
+import Frequency10 from '../assets/workNami/Frequency1.png'
+import Frequency11 from '../assets/workNami/Frequency1.png'
+import { useState, useEffect } from 'react'
 import translate from '../translate.json'
 import styles from '../styles/workNami.module.css'
 
@@ -15,6 +27,9 @@ const WorkNami = () => {
       setX(prevX => !prevX);
     }
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
     return(
         <div>
@@ -28,7 +43,7 @@ const WorkNami = () => {
             <ResumeProject
             banner = {cover}
             title = 'Nami'
-            titleResume= 'resumo'
+            titleResume= {x ? translate.workNami.pagesTitle.text1EN : translate.workNami.pagesTitle.text1PT}
             titleLocation= {x ? 'Nova Serrana, Brazil' : 'Nova Serrana, Brasil'}
             titleData= {x ? 'Apr 2023 - Jun 2024.' : 'Abr 2023 - Jun 2024.'}
             button1='UX Design'
@@ -73,7 +88,50 @@ const WorkNami = () => {
                 <p className={styles.SubTitle}>{x ? 'Quantitative Research' : 'Pesquisa Quantitativa'}</p>
                 <p className={styles.resume}>{x ? translate.workNami.theprocess.research.text10EN : translate.workNami.theprocess.research.text10PT}</p>
             </section>
-            
+            <Frequency
+                img={Frequency1}
+                text={x ? translate.workNami.cards.text1EN : translate.workNami.cards.text1PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency2}
+                text={x ? translate.workNami.cards.text2EN : translate.workNami.cards.text2PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency3}
+                text={x ? translate.workNami.cards.text3EN : translate.workNami.cards.text3PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency4}
+                text={x ? translate.workNami.cards.text4EN : translate.workNami.cards.text4PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency5}
+                text={x ? translate.workNami.cards.text5EN : translate.workNami.cards.text5PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency6}
+                text={x ? translate.workNami.cards.text6EN : translate.workNami.cards.text6PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency7}
+                text={x ? translate.workNami.cards.text7EN : translate.workNami.cards.text7PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency8}
+                text={x ? translate.workNami.cards.text8EN : translate.workNami.cards.text8PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency9}
+                text={x ? translate.workNami.cards.text9EN : translate.workNami.cards.text9PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency10}
+                text={x ? translate.workNami.cards.text10EN : translate.workNami.cards.text10PT}
+            ></Frequency>
+            <Frequency
+                img={Frequency11}
+                text={x ? translate.workNami.cards.text11EN : translate.workNami.cards.text11PT}
+            ></Frequency>
         </div>
     )
 }
